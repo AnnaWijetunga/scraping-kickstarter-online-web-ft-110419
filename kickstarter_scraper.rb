6e78f3d1e2f130547071fb7183a579c25fb03dcb
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-22 17:39:59 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,14 +14,12 @@ end
 
 create_project_hash
 
-# project
 # projects: kickstarter.css("li.project.grid_4")
 
-# title 
 # title: project.css("h2.bbcard_name strong a").text
 
-# image link 
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 
-# description
 # description: project.css("p.bbcard_blurb").text
+
+# location: 
